ccfdbe3632fda14d8358f84e2ee8e16fc6c86627
Henrique Fonseca <h.marquesluiz@gmail.com>
2021-02-27 22:54:21 -0300

    Refatoração da listagem de Hospedes

--- src/modules/hospede/HospedeList.js
+++ src/modules/hospede/HospedeList.js
@@ -1,70 +1,89 @@
-import React from 'react'
-import { MaskedField, TextField } from '../../lib/Fields'
-import { List, Filter } from '../../lib/Components'
-import { useFilter, useListRepository } from '../../lib/Hooks'
-import { Field } from 'react-final-form'
-import Grid from '@material-ui/core/Grid'
-import hospedeRepository from './hospedeRepository'
-import { formatCpf } from '../../utils/Utils'
-import { cpfRegex } from '../../utils/regex'
+import React from "react";
+import { MaskedField, TextField } from "../../lib/Fields";
+import { List, Filter } from "../../lib/Components";
+import { useFilter, useListRepository } from "../../lib/Hooks";
+import { Field } from "react-final-form";
+import Grid from "@material-ui/core/Grid";
+import hospedeRepository from "./hospedeRepository";
+import { formatCpf } from "../../utils/Utils";
+import { cpfRegex } from "../../utils/regex";
+import { Typography } from "@material-ui/core";
 
 export const SEXOS = [
-  { label: 'Masculino', value: 'MASCULINO' },
-  { label: 'Femino', value: 'FEMININO' },
-  { label: 'Outros', value: 'OUTROS' }
-]
+  { label: "Masculino", value: "MASCULINO" },
+  { label: "Femino", value: "FEMININO" },
+  { label: "Outros", value: "OUTROS" },
+];
 
 export const ESTCIVIL = [
-  { label: 'Casado', value: 'CASADO' },
-  { label: 'Solteiro', value: 'SOLTEIRO' },
-  { label: 'Viúvo', value: 'VIUVO' },
-  { label: 'Divorciado', value: 'DIVORCIADO' }
-]
+  { label: "Casado", value: "CASADO" },
+  { label: "Solteiro", value: "SOLTEIRO" },
+  { label: "Viúvo", value: "VIUVO" },
+  { label: "Divorciado", value: "DIVORCIADO" },
+];
 
 const HospedeFilter = () => {
-  const filter = useFilter({})
+  const filter = useFilter({});
   return (
-    <Filter {...filter} labels={{ find: 'Buscar', clear: 'Limpar' }}>
+    <Filter {...filter} labels={{ find: "Buscar", clear: "Limpar" }}>
       <Grid container spacing={2}>
         <Grid item sm={4} xs={12}>
-          <Field name='nomeCompleto' label='Nome Completo' fullWidth component={TextField} />
+          <Field
+            name="nomeCompleto"
+            label="Nome Completo"
+            fullWidth
+            component={TextField}
+          />
         </Grid>
         <Grid item sm={4} xs={12}>
-          <Field name='cpf' label='CPF' fullWidth component={MaskedField} mask={cpfRegex} />
+          <Field
+            name="cpf"
+            label="CPF"
+            fullWidth
+            component={MaskedField}
+            mask={cpfRegex}
+          />
         </Grid>
       </Grid>
     </Filter>
-  )
-}
+  );
+};
 
 const HospedeList = ({ ...props }) => {
   const listOptions = {
     fields: {
       nomeCompleto: {
-        label: 'Nome Completo'
+        label: "Nome Completo",
       },
       cpf: {
-        label: 'CPF',
-        format: cpf => formatCpf(cpf)
+        label: "CPF",
+        format: (cpf) => formatCpf(cpf),
       },
       telefone: {
-        label: 'Telefone'
+        label: "Telefone",
       },
       email: {
-        label: 'E-mail',
+        label: "E-mail",
       },
-    }
-  }
+    },
+  };
 
   const listHook = useListRepository({
     repository: hospedeRepository,
-    defaultSort: 'nomeCompleto',
-    path: 'hospede'
-  })
+    defaultSort: "nomeCompleto",
+    path: "hospede",
+  });
 
   return (
     <>
       <HospedeFilter />
+      <Grid container spacing={2}>
+        <Grid item sm={12}>
+          <Typography variant="h6" color="textPrimary">
+            Lista de Hóspedes
+          </Typography>
+        </Grid>
+      </Grid>
       <List
         {...props}
         {...listHook}
@@ -73,7 +92,7 @@ const HospedeList = ({ ...props }) => {
         listOptions={listOptions}
       />
     </>
-  )
-}
+  );
+};
 
-export default HospedeList 
+export default HospedeList;
