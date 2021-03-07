522716b6cf25a233cdbd93c71757a5e889e35f8b
Diogo Fernando de Melo Sales <diogo@eurekalabs.com.br>
2021-03-07 02:18:33 -0300

    finalizacao do tabs de item quarto na categoria do quarto

--- src/modules/categoria-item-quarto/CategoriaItemQuartoForm.js
+++ src/modules/categoria-item-quarto/CategoriaItemQuartoForm.js
@@ -7,9 +7,11 @@ import { useEntityManager } from '../../lib/Hooks'
 import categoriaitemQuartoRepository from './categoriaitemQuartoRepository'
 import * as yup from 'yup'
 import yupValidation from '../../lib/yupValidation'
+import ItemQuartoAutoComplete from '../item-quarto/ItemQuartoAutoComplete'
 
 const quartoSchema = yup.object().shape({
-    nome: yup.string().required("Obrigatório"),
+    quantidade: yup.number().min(1, "Quantidade minima de 1.").required("Obrigatório"),
+    itemQuartoId: yup.number().required("Obrigatório")
 })
 
 const validate = yupValidation(quartoSchema)
@@ -18,21 +20,38 @@ const CategoriaItemQuartoForm = props => {
 
     const entityManager = useEntityManager({
         repository: categoriaitemQuartoRepository,
+        initialData: {
+            quantidade: 1
+        }
     })
+
+    const handleSubmit = data => {
+        data.categoriaQuartoId = props.match.params.id;
+        return entityManager.onSubmit(data);
+    };
 
     return (
         <CrudForm
             {...props}
             {...entityManager}
             validate={validate}
+            onSubmit={handleSubmit}
             withPaper
         >
             <Grid container spacing={2}>
-                <Grid item sm={12} xs={12}>
+                <Grid item sm={6} xs={12}>
                     <Field
                         fullWidth
-                        name='nome'
-                        label='Nome do item'
+                        name='itemQuartoId'
+                        label='Item'
+                        component={ItemQuartoAutoComplete}
+                    />
+                </Grid>
+                <Grid item sm={6} xs={12}>
+                    <Field
+                        fullWidth
+                        name='quantidade'
+                        label='Quantidade'
                         component={TextField}
                     />
                 </Grid>
--- src/modules/categoria-item-quarto/CategoriaItemQuartoList.js
+++ src/modules/categoria-item-quarto/CategoriaItemQuartoList.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { List } from '../../lib/Components'
+import { autocompleteHelpers } from '../../lib/Common'
 import { useListRepository } from '../../lib/Hooks'
-
 import categoriaitemQuartoRepository from './categoriaitemQuartoRepository'
+const { toOption } = autocompleteHelpers;
+
 
 const CategoriaItemQuartoList = ({ ...props }) => {
   const listOptions = {
@@ -21,14 +23,21 @@ const CategoriaItemQuartoList = ({ ...props }) => {
     repository: categoriaitemQuartoRepository,
     path: 'categoria-item-quarto',
     query: [["categoriaQuartoId", props.match.params.id]],
+    forceRemove: true,
   })
+
+  listHook.onClickEdit = props.onClickEdit;
+  listHook.onClickNew = props.onClickNew;
+  listHook.onClickView = null;
+
+  delete listHook.undoRemove;
+  listHook.deleteFItem = listHook.removeItem;
+  delete listHook.removeItem;
 
   return (
     <>
       <List
-        {...props}
         {...listHook}
-        onClickView={null}
         hideDownloadIcon
         listOptions={listOptions}
       />
